ea5f5273034eb7e8fc79ba538e46f69fdf27e8c6
AleksandraJarosz <56384425+AleksandraJarosz@users.noreply.github.com>
2020-12-12 18:49:49 +0100

    Create Floors1.tsx

---
+++ Floors1.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Floors1" tilewidth="32" tileheight="32" tilecount="2320" columns="40">
+ <image source="Tilesets_32x32.png" width="1280" height="1856"/>
+ <tile id="70">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
