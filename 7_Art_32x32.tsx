b9a304f344cfd2ff2d2029552e52a8fb160b7c1e
AleksandraJarosz <56384425+AleksandraJarosz@users.noreply.github.com>
2020-12-12 18:57:57 +0100

    Create 7_Art_32x32.tsx

---
+++ 7_Art_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="7_Art_32x32" tilewidth="32" tileheight="32" tilecount="160" columns="16">
+ <image source="7_Art_32x32.png" width="512" height="320"/>
+</tileset>
