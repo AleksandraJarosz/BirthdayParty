ec2a6001475beedc46394b854c94e7ed3dd60bab
AleksandraJarosz <56384425+AleksandraJarosz@users.noreply.github.com>
2020-12-12 22:53:41 +0100

    Update Floors1.tsx

--- Floors1.tsx
+++ Floors1.tsx
@@ -1,9 +1,27 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Floors1" tilewidth="32" tileheight="32" tilecount="2320" columns="40">
  <image source="Tilesets_32x32.png" width="1280" height="1856"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="8" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="70">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="253">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+   <object id="2" x="0" y="0" width="32" height="32"/>
+   <object id="5" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
 </tileset>
