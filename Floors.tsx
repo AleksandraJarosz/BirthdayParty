3802ff375ce3104ac3f18d2f46ff24df9032d257
AleksandraJarosz <56384425+AleksandraJarosz@users.noreply.github.com>
2020-12-15 23:30:18 +0100

    Delete Floors.tsx

--- Floors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="Floors" tilewidth="32" tileheight="32" tilecount="2320" columns="40">
- <image source="Tilesets_32x32.png" width="1280" height="1856"/>
-</tileset>
